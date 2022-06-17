6c0ca55a1c2628615cc1999e7a8df5fffdb9d654
Vitor-Kemuel <kemuvitor@gmail.com>
2022-06-17 20:12:25 -0300

    adicionando imagens

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -14,19 +14,19 @@ export class CourseListComponent implements OnInit {
     this.courses = [
       {
         id: 1,
-        name: "Analise e desenvolvimento de sistemas",
-        imageUrl: "url",
+        name: "Angular: Forms",
+        imageUrl: "/assets/images/forms.png",
         price: 15,
         code: "XPG-9756",
         duration: 40,
         rating: 5.4,
         releaseDate: 'december, 15, 2008',
       },
-      
+
       {
         id: 2,
-        name: "Informatica para negocio",
-        imageUrl: "url",
+        name: "Angular: HTTP",
+        imageUrl: "/assets/images/http.png",
         price: 13,
         code: "XPG-9757",
         duration: 40,
